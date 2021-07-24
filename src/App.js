09247df7505c32e31b57622e60847780e0e192b9
Rohit <Rohsh00@gmail.com>
2021-07-24 11:40:09 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react'
 import HomeScreen from './Components/HomeScreen'
 
+
 const App = () => {
-
-const quizStart = () =>{
-  alert("this button is clicked")
-
-}
-
 
   return (
     <>
-  <HomeScreen
-  quiz={quizStart}
-   />
- 
+     <HomeScreen /> 
     </>
   )
 }
